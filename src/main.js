e08953c212ceab7864e5e57e862af7cc6b8e4824
Kayes <kayes@field.buzz>
2019-10-16 01:37:24 +0600

    Added postgresql database and elementui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 
-import store from '@/store' 
+import store from '@/store'
 import router from '@/router'
+import {Table, TableColumn} from 'element-ui'
 
 Vue.config.productionTip = false
 
-// Vue.use(VueRouter)
+Vue.use(Table, TableColumn)
 
 const vue = new Vue({
   router,
